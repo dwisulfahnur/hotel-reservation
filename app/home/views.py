9d1a09c5d85109c33764dae1f6ec38c097acfe39
dwisulfahnur <dwisulfahnur@gmail.com>
2016-03-09 13:49:39 +0800

    modify home module

--- app/home/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request
-
-home_views = Blueprint('home', __name__, template_folder='../templates')
-
-@home_views.route("/")
-def home():
-    return render_template("helloworld.html")
